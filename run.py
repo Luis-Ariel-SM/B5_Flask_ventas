65cdeebc85bf20ecb72ea2c3510f27a47ee8194d
Luis Ariel Santander Méndez <luisarielsantander@gmail.com>
2020-05-26 01:06:53 +0200

    Devuelve jason

--- run.py
+++ run.py
@@ -1,10 +1,32 @@
 from flask import Flask
+import csv
 
 app = Flask (__name__)
+   
         
 @app.route("/")
 def index():
-    return 'Hola mundo'
+    fVentas = open ('./sales10.csv', 'r')
+    csvreader = csv.reader(fVentas, delimiter=',')
+
+    registros = []
+    d = {} #creando el diccionario
+    for linea in csvreader:
+        registros.append(linea)
+        if linea [0] in d:
+            d[linea[0]]['ingresos'] += float(linea[11])
+            d[linea[0]]['beneficios'] += float(linea[13])
+        else:
+            if linea [0] != 'region':
+                d[linea[0]] = {'ingresos': float(linea [11]), 'beneficios': float(linea[13])}
+    
+    return d
+    print (d)
+
+    
+
+
+
 
     
     
